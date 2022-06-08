f16d3d68817a8c8534d8c723e6b450b6e661a431
Biruk Lemma <buraf13k@gmail.com>
2022-06-08 09:26:22 -0700

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
 /**
- * main - Comment causes an infinite loop
- * Return: 0
- */
+* main - Comment causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
-	/* int i; */
 
-	printf("Infinite loop incoming: (\n");
+	/*int i;*/
 
-	/* i = 0; */
+	printf("Infinite loop incoming :(\n");
+
+	/*i = 0;*/
 
 	/* while (i < 10) */
 	/* Infinite loop, there is not increment for i */
