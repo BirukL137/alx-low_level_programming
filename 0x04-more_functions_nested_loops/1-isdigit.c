f8736c24407e5bd8cbba97ead96e6f3639dbc735
Biruk Lemma <buraf13k@gmail.com>
2022-06-09 05:20:07 -0700

    1. isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - function that check if it's a digit
+ * @c: checks the digit
+ * Return: 1 if c is a digit and 0 otherwise
+ */
+
+int main(void)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
