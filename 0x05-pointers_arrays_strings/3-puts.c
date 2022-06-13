e2297407ead0e91fbf677b2cfb994fb6edf19328
Biruk Lemma <buraf13k@gmail.com>
2022-06-12 22:00:33 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - function that prints a string followed by new line
+ * @str: string
+ * Returns: Sucess 0
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+
+	return (0);
+}
