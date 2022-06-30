c6be4ba6af1b264094600f35df48f80ab12b29aa
Biruk Lemma <buraf13k@gmail.com>
2022-06-30 10:21:55 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
-
 /**
  * _is_zero - determines if any number is zero
  * @argv: argument vector.
@@ -15,19 +14,17 @@ void _is_zero(char *argv[])
 	int i, isn1 = 1, isn2 = 1;
 
 	for (i = 0; argv[1][i]; i++)
-		if (argv[1][i] != '0')
-		{
-			isn1 = 0;
-			break;
-		}
-
+	if (argv[1][i] != '0')
+	{
+		isn1 = 0;
+		break;
+	}
 	for (i = 0; argv[2][i]; i++)
-		if (argv[2][i] != '0')
-		{
-			isn2 = 0;
-			break;
-		}
-
+	if (argv[2][i] != '0')
+	{
+		isn2 = 0;
+		break;
+	}
 	if (isn1 == 1 || isn2 == 1)
 	{
 		printf("0\n");
@@ -48,7 +45,7 @@ char *_initialize_array(char *ar, int lar)
 	int i = 0;
 
 	for (i = 0; i < lar; i++)
-		ar[i] = '0';
+	ar[i] = '0';
 	ar[lar] = '\0';
 	return (ar);
 }
@@ -67,23 +64,21 @@ int _checknum(char *argv[], int n)
 	int ln;
 
 	for (ln = 0; argv[n][ln]; ln++)
-		if (!isdigit(argv[n][ln]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
+	if (!isdigit(argv[n][ln]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	return (ln);
 }
 
 /**
  * main - Entry point.
  * program that multiplies two positive numbers.
- * @arc: number of arguments.
+ * @argc: number of arguments.
  * @argv: arguments vector.
- *
  * Return: 0 - success.
  */
-
 int main(int argc, char *argv[])
 {
 	int ln1, ln2, lnout, add, addl, i, j, k, ca;
@@ -105,7 +100,7 @@ int main(int argc, char *argv[])
 			{
 				add = (nout[k] - '0') + addl;
 				if (add > 9)
-					nout [k - 1] = (add / 10) + '0';
+					nout[k - 1] = (add / 10) + '0';
 				nout[k] = (add % 10) + '0';
 			}
 			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
@@ -127,3 +122,4 @@ int main(int argc, char *argv[])
 	printf("%s\n", nout);
 	return (0);
 }
+
