559fe47155099c3087a8272fe2b23646c97280ac
Biruk Lemma <buraf13k@gmail.com>
2022-07-14 10:19:16 -0700

    4. Free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -11,18 +11,18 @@ void free_list(list_t *head)
 
 {
 
-	list_t *temp;
+list_t *temp;
 
-	while (head != NULL)
+while (head != NULL)
 
-	{
+{
 
-		temp = head;
+temp = head;
 
-		head = head->next;
-	
-		free(temp->str);
-		
-		free(temp);
-	}
+head = head->next;
+
+free(temp->str);
+										free(temp);
+										}
+
 }
