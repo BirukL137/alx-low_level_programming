e388f4f7387609a202ed5d7c07f9729f11b13a62
Biruk Lemma <buraf13k@gmail.com>
2022-07-18 16:08:05 -0700

    10. Delete at index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,43 +14,68 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 {
 
-	unsigned int i;
+unsigned int i;
 
 	listint_t *node;
-	
-	listint_t *prev;
 
-	if (head && *head)
-	{
-		prev = NULL;
+		listint_t *prev;
 
-		for (i = 0; i <= index; i++)
 
-		{
-			if (i > 0 && !prev)
-				break;
-			
-			if (i == index)
-			{
-				if (prev && prev->next)
-				{
-					node = prev->next;
-					prev->next = prev->next->next;
-					free(node);
-					return (1);
 
-				}
-				if (!prev)
-				{
-					node = *head;
-					*head = node->next;
-					free(node)
-					return (1);
-				}
-			}
-			prev = i == 0 ? *head : prev->next;
-		}
-	}
-	return (-1);
+			if (head && *head)
+
+					{
+
+			prev = NULL;
+
+										for (i = 0; i <= index; i++)
+
+													{
+
+												if (i > 0 && !prev)
+
+																						break;
+
+																				if (i == index)
+
+																						{
+
+																						if (prev && prev->next)
+
+																															{
+
+																																								node = prev->next;
+
+																																									prev->next = prev->next->next;
+
+																																																		free(node);
+																																																		return (1);
+
+																																																											}
+
+																																	if (!prev)
+
+																																						{
+
+																																												node = *head;
+
+																																																	*head = node->next;
+
+																																																						free(node);
+
+																																																											return (1);
+
+																																																															}
+
+																																				}
+
+																							prev = i == 0 ? *head : prev->next;
+
+																									}
+
+											}
+
+				return (-1);
 
 }
+
