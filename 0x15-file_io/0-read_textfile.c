2ffd54ab47497d1febea78ebf9c161ff486f7469
Biruk Lemma <buraf13k@gmail.com>
2022-07-25 15:00:48 -0700

    0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,36 +11,36 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 {
 
-ssize_t file, fread, fwrite;
-char *totalSize;
+	ssize_t file, fread, fwrite;
+	char *totalSize;
 
-totalSize = malloc(sizeof(char) * letters);
+	totalSize = malloc(sizeof(char) * letters);
 
-if (totalSize == NULL)
+	if (totalSize == NULL)
+		return (0);
 
-return (0);
+	if (filename == NULL)
+		return (0);
+	
+	file = open(filename, O_RDONLY);
 
-if (filename == NULL)
-return (0);
-file = open(filename, O_RDONLY);
+	if (file == -1)
+		return (0);
 
-if (file == -1)
+	fread = read(file, totalSize, letters);
 
-return (0);
+	if (fread == -1)
+		return (0);
 
-fread = read(file, totalSize, letters);
+	fwrite = write(STDOUT_FILENO, totalSize, fread);
 
-if (fread == -1)
-return (0);
+	if (fwrite == -1)
+		return (0);
 
-fwrite = write(STDOUT_FILENO, totalSize, fread);
-if (fwrite == -1)
-return (0);
+	close(file);
 
-close(file);
+	free(totalSize);
 
-free(totalSize);
-
-return (fwrite);
+	return (fwrite);
 
 }
