bfcf0cb010a6da0285adabe404a8d92b98b1f0d8
Biruk Lemma <buraf13k@gmail.com>
2022-07-25 15:13:42 -0700

    2. Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,6 @@ int append_text_to_file(const char *filename, char *text_content)
         return (-1);
     
     file = open(filename, O_APPEND | O_RDWR, 0664);
-    
     if (file == -1)
         return (-1);
 
@@ -24,12 +23,10 @@ int append_text_to_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i]; i++)
             ;
         fwrite = write(file, text_content, i);
-        
-	if (fwrite == -1)
+       	if (fwrite == -1)
             return (-1);
     }
     
     close(file);
-    
     return (1);
 }
